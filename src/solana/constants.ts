29bab47b57d7ffcc8559c61c229c784f7936c471
adarshzpatel <adarshzpatel@gmail.com>
2024-06-28 00:08:40 +0530

    update program id and market

--- src/solana/constants.ts
+++ src/solana/constants.ts
@@ -2,7 +2,7 @@ export const RPC_URL = {
   devnet: "https://api.devnet.solana.com",
 };
 export const FERMI_DEVNET_PROGRAM_ID =
-  "33ZENzbUfMGwZZYQDCj8DEeBKBqd8LaCKnMfQQnMVGFW";
+  "61iWk6RE2TdZXDgKFpfLAsqTswXBdXHfgoE2UVcd6EHJ";
 
 export type MarketType = {
   baseTokenName: string;
@@ -14,16 +14,6 @@ export const MARKETS: MarketType[] = [
   {
     baseTokenName: "JUP",
     quoteTokenName: "USDC",
-    marketPda: "C6YLjpnvz4aUQUzqdqdiZg1nBDPG6XGVxy6FWmWSiFAD",
-  },
-  {
-    baseTokenName: "CWIF",
-    quoteTokenName: "USDC",
-    marketPda: "DgH5EyJK8EUFeYwfKnLU5JVrFbRZUJBiVh3Re5QRTN3p",
-  },
-  {
-    baseTokenName: "KMNO",
-    quoteTokenName: "USDC",
-    marketPda: "2UgASzUhxCpEMqX91MqaYQp1GzSoqDUPLAV3QMb3UKgb",
+    marketPda: "2mkM1bEeurpvfSTjaNoNrNPkNS1qvhLpcpdvgxHvLkDX",
   },
 ];
